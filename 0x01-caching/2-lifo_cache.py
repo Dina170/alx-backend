e4f87514f0bed0f973742d248b5f25a041074545
dina <dinaelhadidy966@gmail.com>
2024-01-24 07:05:20 -0500

    Create a LIFO caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Defines a LIFO caching class"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO caching class"""
+    def __init__(self):
+        super().__init__()
+        self.last_key = ''
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
